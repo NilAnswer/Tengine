34b02bb7cd28150b1a10755aaab0b7f4568cbd65
NilAnswer <150058714+NilAnswer@users.noreply.github.com>
2023-12-27 18:15:23 +0800

    Update mips32r2-linux-gnu.toolchain.cmake

--- toolchains/mips32r2-linux-gnu.toolchain.cmake
+++ toolchains/mips32r2-linux-gnu.toolchain.cmake
@@ -9,5 +9,10 @@ SET (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 
-set(CMAKE_C_FLAGS "-mabi=32")
-set(CMAKE_CXX_FLAGS "-mabi=32")
+set(CMAKE_C_FLAGS "-march=mips32r2 -mabi=32")
+set(CMAKE_CXX_FLAGS "-march=mips32r2 -mabi=32")
+# other needed options
+SET (TENGINE_TOOLCHAIN_FLAG -march=mips32r2 -mabi=32)
+
+# do not skip OpenMP check as default
+SET (TENGINE_FORCE_SKIP_OPENMP OFF)
